e884cd1e57dd16c61f12f31956d27e3d139043a8
lzp-mayun <liuzepeng0200@163.com>
2022-07-20 13:49:48 +0800

    欢迎弹框bug修改

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <!-- <router-view /> -->
-  <router-view #default="{ Component }">
+  <router-view />
+  <!-- <router-view #default="{ Component }">
     <keep-alive>
       <component :is="Component" v-if="keepAlive" />
     </keep-alive>
     <component :is="Component" v-if="!keepAlive" />
-  </router-view>
+  </router-view> -->
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
-let keepAlive = ref(false)
-const route = useRoute()
+// import { ref, watch } from 'vue'
+// import { useRoute } from 'vue-router'
+// let keepAlive = ref(false)
+// const route = useRoute()
 if (document.getElementById('Loading')) document.getElementById('Loading')?.remove();
-watch(()=>route.path,()=> {
-  keepAlive.value = route.meta.keepAlive as boolean
-})
+// watch(()=>route.path,()=> {
+//   keepAlive.value = route.meta.keepAlive as boolean
+// })
 </script>
 <style>
 </style>
