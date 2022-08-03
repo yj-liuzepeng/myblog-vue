e0e7dc3ac1b2a068db267b70016ef7fc53a4e5c6
lzp-mayun <liuzepeng0200@163.com>
2022-08-03 09:42:25 +0800

    备案号跳转

--- src/components/myfooter.vue
+++ src/components/myfooter.vue
@@ -4,50 +4,55 @@
       <div>本网站由Vue3+Node+Element-plus联合驱动</div>
       <div>
         <el-tag class="tag-item" type="danger">对象存储kodo</el-tag>
-        <span class="beian">京ICP备2021039823号-1</span>
+        <span class="beian" @click="jumpToBeian">京ICP备2021039823号-1</span>
         <el-tag class="tag-item">腾讯云</el-tag>
       </div>
       <div>
         本站已经苟且偷生
-        <span style="color: #6be9b0;font-size: 12px;">{{ showTime }}</span>
+        <span style="color: #6be9b0; font-size: 12px">{{ showTime }}</span>
       </div>
       <div>Liuzepeng's blog</div>
     </div>
   </el-row>
 </template>
 
-<script lang='ts' setup>
-import {ref, onMounted, onUnmounted } from 'vue';
-let startDate = new Date('2021/11/24 18:00')
-let s1 = startDate.getTime()
-let timer, s2, total, day, afterDay, hour, afterHour, min, afterMin
-let showTime = ref('')
+<script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from "vue";
+let startDate = new Date("2021/11/24 18:00");
+let s1 = startDate.getTime();
+let timer, s2, total, day, afterDay, hour, afterHour, min, afterMin;
+let showTime = ref("");
 const lifeTime = () => {
   timer = setInterval(() => {
-    s2 = Date.now()
-    total = (s2 - s1) / 1000
-    day = parseInt(String(total / (24 * 60 * 60)))//计算整数天数
-    afterDay = total - day * 24 * 60 * 60//取得算出天数后剩余的秒数
-    hour = parseInt(String(afterDay / (60 * 60)))//计算整数小时数
-    afterHour = total - day * 24 * 60 * 60 - hour * 60 * 60//取得算出小时数后剩余的秒数
-    min = parseInt(String(afterHour / 60))//计算整数分
-    afterMin = parseInt(String(total - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60))//取得算出分后剩余的秒数
-    showTime.value = day + '天' + hour + '小时' + min + '分钟' + afterMin + '秒'
-  }, 1000)
-  
-}
+    s2 = Date.now();
+    total = (s2 - s1) / 1000;
+    day = parseInt(String(total / (24 * 60 * 60))); //计算整数天数
+    afterDay = total - day * 24 * 60 * 60; //取得算出天数后剩余的秒数
+    hour = parseInt(String(afterDay / (60 * 60))); //计算整数小时数
+    afterHour = total - day * 24 * 60 * 60 - hour * 60 * 60; //取得算出小时数后剩余的秒数
+    min = parseInt(String(afterHour / 60)); //计算整数分
+    afterMin = parseInt(
+      String(total - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60)
+    ); //取得算出分后剩余的秒数
+    showTime.value =
+      day + "天" + hour + "小时" + min + "分钟" + afterMin + "秒";
+  }, 1000);
+};
+const jumpToBeian = () => {
+  window.open("https://beian.miit.gov.cn/");
+};
 onMounted(() => {
-  lifeTime()
-})
+  lifeTime();
+});
 onUnmounted(() => {
-  if (timer != null) { //对定时器进行销毁
+  if (timer != null) {
+    //对定时器进行销毁
     clearInterval(timer);
   }
-})
-
+});
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .footer {
   width: 100%;
   margin-top: 30px;
@@ -56,6 +61,7 @@ onUnmounted(() => {
   color: #949393;
   font-size: 14px;
   .beian {
+    cursor: pointer;
     margin: 0 10px;
     color: rgb(63, 63, 206);
   }
