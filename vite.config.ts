b5fc3eb6c1a724757946adf83f425b530c97e623
lzp-mayun <liuzepeng0200@163.com>
2023-02-16 17:01:39 +0800

    打包速度优化

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,19 @@
  * @Date: 2022-03-18 11:36:06
  * @Description: file content
  */
-// vite.config.ts
-
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
+import { visualizer } from "rollup-plugin-visualizer";
+import { autoComplete, Plugin as importToCDN } from "vite-plugin-cdn-import";
 // import viteImagemin from 'vite-plugin-imagemin'
-import {
-  ElementPlusResolver,
-  // AntDesignVueResolver,
-} from "unplugin-vue-components/resolvers";
-
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import viteCompression from "vite-plugin-compression";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
-function pathResolve(dir) {
-  return resolve(__dirname, ".", dir);
-}
+// function pathResolve(dir) {
+//   return resolve(__dirname, ".", dir);
+// }
 export default {
   port: 8989, // 你需要定义的端口号
   open: true,
@@ -61,10 +58,40 @@ export default {
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [
-        ElementPlusResolver(),
-        // AntDesignVueResolver()
+      resolvers: [ElementPlusResolver()],
+    }),
+    // gzip压缩
+    viteCompression({
+      verbose: true, //是否在控制台输出压缩结果
+      disable: false, //是否禁用,相当于开关在这里
+      threshold: 10240, //体积大于 threshold 才会被压缩,单位 b，1b=8B, 1B=1024KB  那我们这里相当于 9kb多吧，就会压缩
+      algorithm: "gzip", //压缩算法,可选 [ 'gzip' , 'brotliCompress' ,'deflate' , 'deflateRaw']
+      ext: ".gz", //文件后缀
+    }),
+    // CDN方式引入包
+    importToCDN({
+      modules: [
+        autoComplete("vue"),
+        autoComplete("axios"),
+        {
+          name: "jquery",
+          var: "jquery", //根据main.js中定义的来
+          path: "dist/jquery.js",
+        },
+        {
+          name: "element-plus",
+          var: "elementPlus", //根据main.js中定义的来
+          version: "1.3.0-beta.1",
+          path: "dist/index.full.js",
+          css: "dist/index.css",
+        },
       ],
+    }),
+    // 打包分析
+    visualizer({
+      emitFile: false,
+      filename: "stats.html", // 分析图生成的文件名
+      open: true, // 如果存在本地服务端口，将在打包后自动展示
     }),
   ],
   css: {
@@ -76,11 +103,39 @@ export default {
     //   },
     // },
   },
+  build: {
+    outDir: "dist", //输出目录名
+    minify: "terser", //压缩方式
+    terserOptions: {
+      compress: {
+        drop_console: true, // 剔除console,和debugger
+        drop_debugger: true,
+      },
+    },
+    // chunkSizeWarningLimit: 1500,大文件报警阈值设置,不建议使用
+    rollupOptions: {
+      output: {
+        //静态资源分类打包
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+        assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+        manualChunks(id) {
+          //静态资源分拆打包
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   optimizeDeps: {
     include: [
       "@kangc/v-md-editor/lib/preview-html",
       "@kangc/v-md-editor/lib/theme/vuepress.js",
-      // 'prismjs/prism.js',
     ],
   },
   server: {
